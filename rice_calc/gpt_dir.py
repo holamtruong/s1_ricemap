961d2aae7a5bb50417b9e5811748579a7782fb27
avunguyen93 <vunva93@gmail.com>
2020-08-31 09:48:29 +0700

    config for main

--- rice_calc/gpt_dir.py
+++ rice_calc/gpt_dir.py
@@ -11,12 +11,13 @@ sp.check_call(cmd_pts)
 """
 import subprocess as sp
 import os
+import gdal, ogr
 def S1_process(in_path,out_path):
     gpt = 'C:/Program Files/snap/bin/gpt.exe'
-    graph_path = '/s1_ricemap/rice_calc/graph_ARD.xml'
-    shp='/s1_ricemap/sample_data/tile.shp'
+    graph_path = 'F:/DEV/s1_ricemap/rice_calc/graph_ARD.xml'
+    shp='F:/DEV/s1_ricemap/sample_data/shp/tile.shp'
     file_list=[img for img in os.listdir(in_path) if img[-4:] == '.zip']
-    for i in file_list:
+    for i in range(0,len(file_list)):
         in_name='-Pinput='+os.path.join(in_path,file_list[i])
         out_name='-Poutput='+os.path.join(out_path,file_list[i][17:25]+'_'+file_list[i][63:67]+'_AO_ML_CL_TC'+'{}').format('')
         file_name=file_list[i][17:25]+'_'+file_list[i][63:67]+'_AO_ML_CL_TC'+'{}'.format('.tif')
@@ -29,8 +30,8 @@ def S1_process(in_path,out_path):
         check_path=os.path.join(out_path,file_name)
         check_out=os.path.join(out_path,file_out)
         options = gdal.WarpOptions(cutlineDSName=shp, cropToCutline=True)
-        result_img = gdal.Warp(srcDSOrSrcDSTab=inputPath + image,
-            destNameOrDestDS=outputPath + image[:-4] + '_clip' + image[-4:],
+        result_img = gdal.Warp(srcDSOrSrcDSTab=check_path,
+            destNameOrDestDS=check_out,
             options=options)
         result_img = None
         os.remove(check_path)
